72d6ef54b49542505cfc9d4491c8e29c6714f359
williamdaniel <williamdanielx@gmail.com>
2022-11-09 15:57:45 -0500

    reproduccion de video lenta

--- components/Game/Score.js
+++ components/Game/Score.js
@@ -5,7 +5,7 @@ import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Score({ player }) {
-    const { setscoreOne, setscoretwo } = useContext(ScoreContext)
+    const { setscoreOne, scoreOne, setscoretwo, scoretwo } = useContext(ScoreContext)
 
     const { height, width } = useWindowDimensions();
     const [point, setpoint] = useState(0)
@@ -13,29 +13,34 @@ export default function Score({ player }) {
 
     useEffect(() => {
         if (player === 1) {
-            setscoreOne(point)
             setcolor('#fcfffa')
         } else {
-            setscoretwo(point)
             setcolor('#ff9f1a')
         }
-    }, [point])
+    }, [])
 
+    function _point(add) {
+        if (player === 1) {
+            setscoreOne(scoreOne + add)
+        } else {
+            setscoretwo(scoretwo + add)
+        }
+    }
 
     return (
         <View style={{ backgroundColor: color, flex: 1 }}>
             <View style={styles.points} >
-                <TouchableOpacity onPress={() => setpoint(point + 1)}>
+                <TouchableOpacity onPress={() => _point( + 1)}>
                     <Ionicons name="add-circle" size={50} color="black" />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => setpoint(point - 1)}>
+                <TouchableOpacity onPress={() => _point( - 1)}>
                     <Entypo name="circle-with-minus" size={50} color="black" />
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={() => setpoint(point + 3)}>
+            <TouchableOpacity onPress={() => _point( + 3)}>
                 <Text style={styles.text}>+ 3</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => setpoint(point + 5)}>
+            <TouchableOpacity onPress={() => _point( + 5)}>
                 <Text style={styles.text}>+ 5</Text>
             </TouchableOpacity>
         </View>
--- screens/VarScreen.js
+++ screens/VarScreen.js
@@ -1,39 +1,136 @@
-import { StyleSheet, Text, View, Pressable, useWindowDimensions } from 'react-native'
+import {
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Animated,
+    PanResponder,
+    useWindowDimensions
+} from 'react-native'
 import React, { useContext, useRef, useState } from 'react'
 import { Video } from 'expo-av';
 import { NavContext } from '../context/NavContext';
 import { VarContext } from '../context/VarContext';
 import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const pointsDistance = ([xA, yA], [xB, yB]) => {
+    return Math.sqrt(
+        Math.pow(xA - xB, 2) + Math.pow(yA - yB, 2)
+    );
+};
 
 export default function VarScreen() {
     const { setpage } = useContext(NavContext)
     const { videoGame } = useContext(VarContext)
-    const {width, height} = useWindowDimensions();
+
     const [status, setStatus] = useState({});
+    const [speed, setspeed] = useState(1)
 
     const video = useRef(null);
 
+    const dimensions = useWindowDimensions();
+
+    const pan = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+    const scale = useRef(new Animated.Value(1)).current;
+
+    const panResponder = useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onMoveShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gestureState) => {
+
+                const activeTouches = event.nativeEvent.changedTouches.length;
+
+                if (activeTouches === 1) {
+                    pan.setValue({
+                        x: gestureState.dx,
+                        y: gestureState.dy,
+                    });
+                } else if (activeTouches >= 2) {
+                    const touches = event.nativeEvent.changedTouches;
+
+                    const touchA = touches[0];
+                    const touchB = touches[1];
+
+                    const distance = pointsDistance(
+                        [touchA.pageX, touchA.pageY],
+                        [touchB.pageX, touchB.pageY]
+                    );
+
+                    const screenMovedPercents = distance / dimensions.width;
+
+                    scale.setValue(1 + screenMovedPercents * 3);
+                }
+            },
+            onPanResponderRelease: () => {
+                Animated.parallel([
+                    Animated.spring(pan, {
+                        toValue: {
+                            x: 0,
+                            y: 0,
+                        },
+                        useNativeDriver: true,
+                    }),
+                    Animated.spring(scale, {
+                        toValue: 1,
+                        useNativeDriver: true,
+                    }),
+                ]).start();
+            },
+        })
+    ).current;
+
     return (
         <View style={styles.container}>
-            <Pressable
-                style={styles.backGame}
-                onPress={() => setpage('billar')}
+            <View style={{ justifyContent: 'space-evenly' }}>
+                <TouchableOpacity
+                    onPress={() => setpage('billar')}
+                >
+                    <Entypo name="back" size={30} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setspeed(1)}
+                >
+                    <MaterialCommunityIcons name="play-speed" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setspeed(0.5)}
+                >
+                    <MaterialCommunityIcons name="speedometer-slow" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setspeed(0.25)}
+                >
+                    <MaterialIcons name="slow-motion-video" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <Animated.View
+                style={{
+                    flex: 1,
+                    height: dimensions.height,
+                    width: dimensions.width,
+                    transform: [
+                        { translateX: pan.x },
+                        { translateY: pan.y },
+                        { scale },
+                    ],
+                }}
+                {...panResponder.panHandlers}
             >
-                <Entypo name="back" size={30} color="black" />
-            </Pressable>
-            <View style={styles.video}>
                 <Video
                     ref={video}
-                    style={{height: height * 0.89, width}}
+                    style={{ flex: 1 }}
                     source={{
                         uri: videoGame,
                     }}
                     useNativeControls
+                    rate={speed}
                     resizeMode="contain"
                     isLooping={false}
                     onPlaybackStatusUpdate={status => setStatus(() => status)}
                 />
-            </View>
+            </Animated.View>
         </View>
     )
 }
@@ -42,14 +139,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#588157',
-    },
-    video: {
-        alignItems: 'center',
-        justifyContent: 'center',
         flexDirection: 'row'
     },
     backGame: {
         padding: 3
     },
-   
+
 })
